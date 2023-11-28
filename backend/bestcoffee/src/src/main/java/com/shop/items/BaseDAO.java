51a57f3525c1da449a0d493bfec842c3680f7a6a
Xina <a4xyiou7vai2th1y6xonlyo06gzejv@bots.bitbucket.org>
2023-11-28 23:51:59 +0000

    add util function is_empty() getIndex for main2.js

---
+++ backend/bestcoffee/src/src/main/java/com/shop/items/BaseDAO.java
@@ -0,0 +1,114 @@
+package com.shop.items;
+
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public abstract class BaseDAO { 
+	/**
+	 * Encapsulation to simplify non-DQL statements
+	 * @param sql SQL statement with placeholders
+	 * @param params Values for the placeholders; the values passed must match the positions of '?' in the SQL statement
+	 * @return Number of affected rows after execution
+	 * @throws Exception 
+	 */
+	public int excuteUpdate(String sql, Object... params) throws Exception {
+		// get connection
+		Connection connection = jdbcUtils.getConnection();
+		
+		PreparedStatement preparedStatement = connection.prepareStatement(sql);
+		// set placeholder
+		// Variable arguments can be used as an array. Database indexing starts from 1, 
+		// so it is necessary to iterate starting from 1.
+		for (int i = 1; i <= params.length; i++) {
+			preparedStatement.setObject(i, params[i - 1]);
+		}
+		
+		int rows = preparedStatement.executeUpdate();
+		
+		preparedStatement.close();
+		
+		if (connection.getAutoCommit()) {
+			jdbcUtils.freeConnection(connection);		
+		}
+		
+		return rows;
+	}
+	
+	/**
+	 * Encapsulation method for non-DQL statements -> Return type is fixed as int.
+	 * Encapsulation for DQL syntax statements -> Return type is generic List<T>.
+	 * 
+	 * <T> declares a generic, an undetermined type.
+	 * 1. To determine the generic type, for example, User.class, T = User.
+	 * 2. Reflection techniques are used for attribute assignment.
+	 * public <T> List<T> executeQuery(Class<T> clazz, String sql, Object... params);
+	 * @throws Exception
+	 */
+	
+	@SuppressWarnings("deprecation")
+	public <T> List<T> executeQuery(Class<T> clazz, String sql, Object... params) throws Exception{
+		// 获取链接
+		Connection connection = jdbcUtils.getConnection();
+		
+		PreparedStatement preparedStatement = connection.prepareStatement(sql);
+		
+		if (params != null && params.length != 0) {
+			for (int i = 1; i <= params.length; i++) {
+				preparedStatement.setObject(i, params[i - 1]);
+			}
+		}
+		
+		ResultSet resultSet = preparedStatement.executeQuery();
+		
+		// store the result
+		List<T> list = new ArrayList<>();
+		
+		// get column information
+		ResultSetMetaData metaData = resultSet.getMetaData();
+		
+		int columnCount = metaData.getColumnCount();
+		while (resultSet.next()) {
+			T t = clazz.newInstance(); // invoke the no parameter constructor to instaniate object
+			
+			// start from 1
+			for (int i = 1; i <= columnCount; i++) {
+				
+				// property
+				Object valueObject = resultSet.getObject(i);
+				// name of propery
+				String propertyName = metaData.getColumnLabel(i);
+				
+				// Reflectively assign values to the object's properties.
+				java.lang.reflect.Field field = clazz.getDeclaredField(propertyName);
+				field.setAccessible(true); // Properties can be set, breaking the private modifier.
+
+		        // issues : bigDecimal come from datebase decimal 
+				// big Decimal can not automatically trans to double
+		        if (valueObject != null) {
+		            if (field.getType() == double.class || field.getType() == Double.class) {
+		                field.set(t, ((BigDecimal) valueObject).doubleValue());
+		            } else {
+		                // rest situations
+		                field.set(t, valueObject);
+		            }
+		        }
+			}
+			list.add(t);
+		}
+		
+		// close resouces
+		resultSet.close();
+		preparedStatement.close();
+		if (connection.getAutoCommit()) {
+			jdbcUtils.freeConnection(connection);
+		}
+		
+		return list;
+	}
+}
---
+++ backend/bestcoffee/src/src/main/java/com/shop/server/MenuMangeServlet.java
@@ -0,0 +1,130 @@
+package com.shop.server;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import com.google.gson.Gson;
+import com.shop.items.MenuDAO;
+import com.shop.items.MenuItem;
+import com.shop.items.MenuObjs;
+
+/**
+ * Servlet implementation class MenuMangeServlet
+ */
+@WebServlet(urlPatterns = {"/menu/*"})
+public class MenuMangeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	MenuDAO menuDAO = new MenuDAO();
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MenuMangeServlet() {
+        // TODO Auto-generated constructor stub
+    }
+    public String getJSONFromReque(HttpServletRequest request) {
+        String reString = "";
+    	// 读取请求的输入流
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(request.getInputStream()))) {
+            StringBuilder requestBody = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                requestBody.append(line);
+            }
+
+            // requestBody.toString() 包含了从客户端发送的 JSON 数据
+            reString = requestBody.toString();
+            System.out.println("Received JSON data: " + reString);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+		return reString;
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		getJSONFromReque(request);
+        String pathInfo = request.getPathInfo();
+
+        if (pathInfo == null || pathInfo.equals("/")) {
+            try {
+				MenuObjs menuObjs = menuDAO.GetMenuData();
+				String responseData = menuObjs.toString();
+		         // 设置响应的内容类型
+		         response.setContentType("application/json");
+		         // 设置响应状态码
+		         response.setStatus(HttpServletResponse.SC_OK);
+		         // 将 JSON 数据写入响应流
+		         response.getWriter().write(responseData);
+		         response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+		         response.setHeader("Access-Control-Allow-Credentials", "true");
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}    
+            
+        } else {
+            // Extract path parameters for "/menu/{id}"
+            String[] pathParts = pathInfo.split("/");
+            if (pathParts.length >= 2) {
+				try {
+	                String id = pathParts[1];
+	                int index = Integer.parseInt(id);
+					MenuItem menuItem = menuDAO.GetMenuProductById(index);
+					Gson gson = new Gson();
+					String responseData = gson.toJson(menuItem);
+			         // 设置响应的内容类型
+			         response.setContentType("application/json");
+			         // 设置响应状态码
+			         response.setStatus(HttpServletResponse.SC_OK);
+			         // 将 JSON 数据写入响应流
+			         response.getWriter().write(responseData);
+			         response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+			         response.setHeader("Access-Control-Allow-Credentials", "true");
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+            } else {
+                response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid path format");
+            }
+        }
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// doGet(request, response);
+		String jsonString = getJSONFromReque(request);
+		try {
+			Gson gson = new Gson();
+			MenuItem menuItem = gson.fromJson(jsonString, MenuItem.class);
+			MenuDAO menuDAO = new MenuDAO();
+		
+			menuDAO.addOrderToMenu(menuItem);
+	         // 设置响应的内容类型
+	         response.setContentType("application/json");
+	         // 设置响应状态码
+	         response.setStatus(HttpServletResponse.SC_OK);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			
+			e.printStackTrace();
+		}
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+	}
+
+}
